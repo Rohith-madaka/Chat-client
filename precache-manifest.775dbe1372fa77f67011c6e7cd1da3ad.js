eeaff74c1fe2aeab470af4ab516c610c85d7081c
Rohith madaka <madaka.rohith.reddy@gmail.com>
2020-08-22 21:28:01 +0530

    Updates

---
+++ precache-manifest.775dbe1372fa77f67011c6e7cd1da3ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "390f24346ae629aa10f21311984bd079",
+    "url": "/chat-client/index.html"
+  },
+  {
+    "revision": "96bd1dd489ad62294846",
+    "url": "/chat-client/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "204f8f48c5183d45b520",
+    "url": "/chat-client/static/js/2.6cf5c528.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chat-client/static/js/2.6cf5c528.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96bd1dd489ad62294846",
+    "url": "/chat-client/static/js/main.e3d59e40.chunk.js"
+  },
+  {
+    "revision": "a0a7092f76b084ef7dc0",
+    "url": "/chat-client/static/js/runtime-main.53cf3b3b.js"
+  }
+]);
